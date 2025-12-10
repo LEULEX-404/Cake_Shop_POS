90e8d3bfdb9c85504ebffca768f5a0a6f0c905c2
Imogirl <it23595576@my.sliit.lk>
2025-12-10 18:07:31 +0530

    admin parts

--- Backend/obj/Debug/net10.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net10.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a8aeee4fec752a7c53792615ff38b594c580b84")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50e501f4f34d79e0b13abfb5b2b93ef66aa6b824")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
