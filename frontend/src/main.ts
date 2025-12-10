8a5b437c9945a2a99ffef3d019f41967c6ba5994
Miyuru Tharuka <miyurut20@gmail.com>
2025-12-10 17:27:12 +0530

    Fix

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,16 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { routes } from './app/app.routes';
+import { provideRouter } from '@angular/router';
 
-bootstrapApplication(App, appConfig)
+bootstrapApplication(App, {
+  ...appConfig,
+  providers: [
+    ...(appConfig.providers || []),
+    provideHttpClient(withFetch()),// << enable fetch API
+    provideRouter(routes)  
+  ]
+})
   .catch((err) => console.error(err));
